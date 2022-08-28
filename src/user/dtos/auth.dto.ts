ffc06eb28fe47a76c730dc48de88efe881d3be7a
Moein Moein Nia <moein@proai.local>
2022-08-28 17:45:32 +0430

    auth user added

---
+++ src/user/dtos/auth.dto.ts
@@ -0,0 +1,10 @@
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
+export class SignUpDto {
+  @IsString()
+  name: string;
+
+  phone: string;
+  email: string;
+  password: string;
+}
