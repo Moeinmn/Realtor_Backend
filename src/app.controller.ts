aee06505257ed9512e8f80f4dc9aee7486529a09
Moein Moeinnia <moeinnia.protonmail.com>
2022-08-29 21:02:56 +0430

    [DB] prisma schema added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,9 @@ import { Controller, Get, Param } from "@nestjs/common";
 @Controller("test")
 export class AppController {
   @Get(":id")
-  getAllExpense(@Param("id") x: string): string {
+  getAllExpense(
+    @Param("id") x: string
+    ): string {
     return x;
   }
 }
